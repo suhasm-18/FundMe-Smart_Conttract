d993219a5ef47f31dc2957f4af33aed7ca8f00e9
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-05-05 22:53:13 +0530

    Update PriceConverter.sol

--- PriceConverter.sol
+++ PriceConverter.sol
@@ -14,3 +14,9 @@ library PriceConverter {
        AggregatorV3Interface priceFeed =  AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
        return priceFeed.version();
     }
+function getConversionRate(uint256 ethAmount) internal view returns(uint256){
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+}
